c7fc8e29fb8d249884358b27bef6ed566a37e081
tianayj <tianwyj@gmail.com>
2021-05-04 12:20:38 -0400

    Finished the code to retrieve all the property information for a single product using its productID. It does not yet retrieve the description or model year however.

---
+++ Marketplace_portal/Controllers/ProductSummaryController.cs
@@ -0,0 +1,28 @@
+﻿using Marketplace_portal.Models;
+using MarketplacePortal_Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marketplace_portal.Controllers
+{
+    public class ProductSummaryController : Controller
+    {
+        // GET: ProductSummary
+        public ActionResult Index()
+        {
+            ProductSummaryService productSummaryService = new ProductSummaryService();
+            ProductSummaryModel properties = new ProductSummaryModel
+            {
+                //Key is propertyID, Value is [PropertyName, IsType, Value, HasMinMax, Max, Min]
+                description = productSummaryService.getDescription(1), //Change this 1 to productID that's passed in.
+                properties = productSummaryService.getProperties(1) //Change this 1 to productID that's passed in.
+            };
+
+            ViewData["propertiesObject"] = properties;
+            return View();
+        }
+    }
+}
