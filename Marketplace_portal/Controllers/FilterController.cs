5b77aac7b4d5333d1bab0d15ccfadd27b174216d
bashaashma <bashaashma@gmail.com>
2021-05-05 22:04:33 -0400

    linking pages

--- Marketplace_portal/Controllers/FilterController.cs
+++ Marketplace_portal/Controllers/FilterController.cs
@@ -18,7 +18,15 @@ namespace Marketplace_portal.Controllers
         List<string> types = new List<string>();
 
 
-        public ActionResult Test( int[] Ids)
+        public ActionResult Subcategory()
+        {
+            //Request["txtAmoun"].ToString();
+            return RedirectToAction("Fans", "Filter");    
+            
+        }
+
+
+            public ActionResult Test( int[] Ids)
         {
             /*string id1 = Request.QueryString["id1"].ToString();
             string id2 = Request.QueryString["id2"].ToString();
